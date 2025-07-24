7316f3422c5f87c5a31f4e0c796df6e8968fa4a8
BHARATH <BHARATH@Bharath-Saravanan>
2025-07-24 14:01:51 +0530

    Day Three Completed

--- Capgemini/src/Day3/Encapsulation/EncapsulationDemo.java
+++ Capgemini/src/Day3/Encapsulation/EncapsulationDemo.java
@@ -1,9 +1,15 @@
 package Day3.Encapsulation;
 
 public class EncapsulationDemo {
-
-	public EncapsulationDemo() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
 	}
 
 }
